18f031e0bafbbf19eb3bdfa8a136e968e23fffbb
YoungJoong <2403110262@office.kopo.ac.kr>
2025-08-29 19:45:11 +0900

    FIRST COMMIT

---
+++ src/main/java/com/poly/polybank/PolybankApplication.java
@@ -0,0 +1,13 @@
+package com.poly.polybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolybankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolybankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poly/polybank/PolybankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.polybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolybankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
